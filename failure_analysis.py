910a85a460ec412b8e2578a1302f14007c7b80ea
Shuchita Mishra <71638384+shuchita28@users.noreply.github.com>
2021-03-23 04:11:42 -0400

    Update failure_analysis.py

--- failure_analysis.py
+++ failure_analysis.py
@@ -2,9 +2,9 @@ import pprint
 import matplotlib.pyplot as pt
 import pandas as pd
 
-data = pd.read_csv('/Users/shuchitamishra/Desktop/zipcar/failures.csv')
+data = pd.read_csv('/Users/shuchitamishra/Desktop/Prod-Migration/failures-22-03.csv')
 
-print(data)
+#print(data)
 
 total = len(data)
 
@@ -16,36 +16,31 @@ missing_key_error    = data.error_msg.str.count("missing_key").sum()
 
 other_errors         = total-(interrupted_error+RollBack_error+forced_reset_error+market_id_error+missing_key_error)
 
-print("------------------------------RESULTS--------------------------------------------")
-print("Unable to get user from members due to: interrupted: members/user >> null   ", interrupted_error)
+print("Total number of Failed accounts:", total)
 print("---------------------------------------------------------------------------------")
-print("RollBackInfo(....stepThatFailed=CREATE_IDP_USER)                            ",RollBack_error)
-print("---------------------------------------------------------------------------------")
-print("An unexpected response from /users/forced_reset_status                      ",forced_reset_error)
-print("---------------------------------------------------------------------------------")
-print("illegal_argument: membership/account/migrate >> Missing 'market_id'         ",market_id_error)
-print("---------------------------------------------------------------------------------")
-print("Migration failure -  members_user_missing_key: Invalid/Missing key 'status' ",missing_key_error)
-print("---------------------------------------------------------------------------------")
-print("Other errors                                                                ",other_errors)
+
+e1 = ['interrupted_error','RollBackInfo_error','forced_reset_error','market_id_error', 'missing_key_error','Other']
+e2 = [interrupted_error,RollBack_error,forced_reset_error,market_id_error, missing_key_error,other_errors]
+e3 = 
+errors = pd.DataFrame(list(zip(e1,e2)))
+errors.columns = ['error_type','Count']
+filtered_errors = errors[(errors.Count != 0)]
+print(filtered_errors)
 print("---------------------------------------------------------------------------------")
 
 
-analysis = [('interrupted_error', interrupted_error),
-            ('RollBackInfo_error', RollBack_error),
-            ('forced_reset_error', forced_reset_error),
-            ('market_id_error', market_id_error),
-            ('missing_key_error', missing_key_error),
-            ('other', other_errors)
-            ]
 
-pprint.pprint(analysis)
-
-sizes,labels = [i[1] for i in analysis], [i[0] for i in analysis]
-
-pt.pie(sizes, labels = labels, autopct='%1.1f%%')
-
-pt.legend(title="Failure Analysis")
+values = filtered_errors['Count']
+values = values.to_list()
+#print(type(values))
+labels = filtered_errors['error_type']
+labels = labels.to_list()
+#print(type(labels))
+pt.pie(values, labels = labels, autopct='%1.1f%%')
+pt.title('Failed Accounts Analysis')
 
 pt.show()
+
+ 
+
  
