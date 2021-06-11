0a9f364286f680725a6d03aaedf2cfb4c1da548e
Shuchita Mishra <71638384+shuchita28@users.noreply.github.com>
2021-06-11 18:29:51 +0530

    Automate skipped error analysis

--- skipped_analysis.py
+++ skipped_analysis.py
@@ -5,6 +5,10 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
+def find_error_in_df(df,acct):
+    presentCheck = df[df['account_number']==acct].index.values.astype(int)[0]
+    print(df.loc[presentCheck,['error_msg']])
+
 def Diff(li1, li2):
     li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2]
     return li_dif
@@ -76,9 +80,9 @@ for i in e1:
 other_errors = total-(sum(e2))
 
 
-print("---------------------------------------------------------------------------------")
+print("-----------------------------------------------------------------------------------------------------")
 print("Total number of Skipped accounts:", total)
-print("---------------------------------------------------------------------------------")
+print("-----------------------------------------------------------------------------------------------------")
 
 
 errors = pd.DataFrame(list(zip(e1,e2,e3)))
@@ -86,16 +90,20 @@ errors.columns = ['error_type','Count','Account_id']
 filtered_errors = errors[(errors.Count != 0)]
 
 print(filtered_errors)
-print('Other errors       ',other_errors)
+if(other_errors>=0):
+    print('New errors       ',other_errors)
 #print("---------------------------------------------------------------------------------")
 #print(e3)
 
 
 li3 = Diff(e4, e5)
-print("These accounts are difference:")
+print("These accounts were skipped due to some new condition:")
 print(li3)
 print(len(li3))
 
+for i in li3:
+#   print(i)
+    find_error_in_df(data2,i)
 
 values = filtered_errors['Count']
 values = values.to_list()
@@ -104,6 +112,12 @@ labels = filtered_errors['error_type']
 labels = labels.to_list()
 #print(type(labels))
 
+"""
+pt.pie(values, labels = labels, autopct='%1.1f%%')
+pt.title('Failed Accounts Analysis')
+
+pt.show()
+"""
 # creating the bar plot
 #Figure size
 fig , ax = pt.subplots(figsize = (16, 9))
@@ -153,3 +167,13 @@ fig.text(0.9, 0.15, 'QA @ Zipcar, Inc.', fontsize = 12,
 pt.tight_layout()
 # Show Plot
 pt.show()
+
+"""
+pt.bar(labels, values, color ='maroon',
+        width = 0.4)
+
+pt.xlabel("Error Types")
+pt.ylabel("No. of accounts failed")
+pt.title("Account failure reasons")
+pt.show()
+"""
